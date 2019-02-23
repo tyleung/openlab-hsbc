83976ff5e2c74b910c886e50f62114f1c3f2a8a8
tyleung <tyleung@ualberta.ca>
2019-02-24 06:45:14 +0800

    Fix clearinterval on api call

--- src/infoSection.js
+++ src/infoSection.js
@@ -10,7 +10,11 @@ class InfoSection extends Component {
   componentDidUpdate(prevProps) {
     if (prevProps.circleCenter === this.props.circleCenter) {
       // do nothing
-    } else if (this.props.circleCenter !== undefined) {
+    } else if (
+      this.props.circleCenter !== undefined &&
+      this.props.circleCenter !== prevProps.circleCenter
+    ) {
+      clearInterval(this.intervalId);
       this.intervalId = setInterval(() => {
         getRealtimeData().then(option => this.setState({ option }));
       }, 1000);
@@ -27,7 +31,7 @@ class InfoSection extends Component {
     return (
       <div className="info-section">
         <span className="info-section-text">
-          Click an area on the map or search by category
+          Click an area on the map or select a category
         </span>
         <div className="info-section-charts">
           <div className="info-section-chart-top" />
