21e9c175f34b9b9eaae34230ba33b3199cd06a62
tyleung <tyleung@ualberta.ca>
2019-02-24 02:21:39 +0800

    Add echarts

---
+++ src/echartsContainer.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import ReactEcharts from 'echarts-for-react';
+
+class EchartsContainer extends Component {
+  render() {
+    const option = {
+      title: {
+        text: 'chart for age distribution',
+        left: 'center',
+        textStyle: {
+          color: '#111',
+          fontStyle: 'normal',
+          fontWeight: 'bold',
+          fontFamily: 'sans-serif',
+          fontSize: 22
+        }
+      },
+      xAxis: {
+        type: 'category',
+        data: [
+          '[0, 10)',
+          '[21, 30)',
+          '[15, 21)',
+          '[10, 15)',
+          '[30, 35)',
+          '[35, 100)'
+        ]
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [2578, 2249, 1603, 1293, 1222, 1055],
+          type: 'bar'
+        }
+      ]
+    };
+    return <ReactEcharts option={option} />;
+  }
+}
+
+export default EchartsContainer;
--- src/infoSection.js
+++ src/infoSection.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import EchartsContainer from './echartsContainer';
 
 class InfoSection extends Component {
   render() {
     return (
       <div className="info-section">
         Click an area on the map or search for a product
+        <EchartsContainer />
       </div>
     );
   }
