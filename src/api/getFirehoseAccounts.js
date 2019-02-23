5dc446b2a25cd94b710baae1e328c2d81fa8dd07
tyleung <tyleung@ualberta.ca>
2019-02-23 21:37:12 +0800

    Base api call to retrieve transactions from click location

--- src/api/getFirehoseAccounts.js
+++ src/api/getFirehoseAccounts.js
@@ -1,26 +1,33 @@
 import axios from 'axios';
 
 export function getFirehoseAccounts() {
-    return axios.get("/v3.0.0/banks/hsbc.02.hk.hsbc/firehose/accounts/views/owner", {
-        headers: {
-            Authorization: `DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.8FcOstmReHMHIgi18nJCe_FIX2Pfic4_VizrYL2uEBg"`
-        }
-    }).then(response => {
-        if(response && response.status == 200) {
-            return response.data
-        }
+  return axios
+    .get('/v3.0.0/banks/hsbc.02.hk.hsbc/firehose/accounts/views/owner', {
+      headers: {
+        Authorization: `DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.8FcOstmReHMHIgi18nJCe_FIX2Pfic4_VizrYL2uEBg"`
+      }
+    })
+    .then(response => {
+      if (response && response.status === 200) {
+        return response.data;
+      }
     });
-};
+}
 
 export function getFirehoseTransactionsForAccount(accountId) {
-    return axios.get(`/v3.0.0/banks/hsbc.02.hk.hsbc/firehose/accounts/${accountId}/views/owner/transactions`, {
+  return axios
+    .get(
+      `/v3.0.0/banks/hsbc.02.hk.hsbc/firehose/accounts/${accountId}/views/owner/transactions`,
+      {
         headers: {
-            Authorization: `DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.8FcOstmReHMHIgi18nJCe_FIX2Pfic4_VizrYL2uEBg"`
+          Authorization: `DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.8FcOstmReHMHIgi18nJCe_FIX2Pfic4_VizrYL2uEBg"`
         }
-    }).then(response => {
-        if(response && response.status == 200) {
-            return response.data
-        }
+      }
+    )
+    .then(response => {
+      if (response && response.status === 200) {
+        return response.data;
+      }
     });
 }
 
@@ -30,6 +37,8 @@ export function getFirehoseTransactionsForAccount(accountId) {
 //     })
 // })
 
-getFirehoseTransactionsForAccount("5e420e0e-f2ce-4f03-a67c-a1075343dc24").then(data => {
+getFirehoseTransactionsForAccount('5e420e0e-f2ce-4f03-a67c-a1075343dc24').then(
+  data => {
     console.log(data);
-})
+  }
+);
