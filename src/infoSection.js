2b39d485512fd2e56a68d7a1321ea6e280561a2f
tyleung <tyleung@ualberta.ca>
2019-02-24 00:26:20 +0800

    styling

--- src/infoSection.js
+++ src/infoSection.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 class InfoSection extends Component {
   render() {
-    return <div className="info-section">charts</div>;
+    return (
+      <div className="info-section">
+        Click an area on the map or search for a product
+      </div>
+    );
   }
 }
 
