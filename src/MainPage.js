948f0cae365c7e023c4b0854539bee4b6c4daf2b
tyleung <tyleung@ualberta.ca>
2019-02-24 00:18:16 +0800

    Add circle

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react';
+import { Circle } from 'react-google-maps';
 import GMap from './gmap/gmap';
+import InfoSection from './infoSection';
 import { getTransactionsAroundLocation } from './api/transactions';
 
 class MainPage extends Component {
+  state = { circleCenter: undefined };
+
   onClick = e => {
     const latlng = { lat: e.latLng.lat(), lng: e.latLng.lng() };
-    getTransactionsAroundLocation(latlng).then(transactions =>
-      console.log(transactions)
+    //getTransactionsAroundLocation(latlng)
+    //  .then(transactions => console.log(transactions))
+    //  .then(() => {
+    console.log(latlng);
+    this.setState({ circleCenter: latlng });
+    //  });
+  };
+
+  renderCircle = center => {
+    return (
+      <Circle
+        center={center}
+        radius={500}
+        strokeColor="#FF0000"
+        strokeOpacity={0.8}
+        strokeWeight={2}
+        fillColor="#FF0000"
+        editable={true}
+      />
     );
   };
 
   render() {
     return (
-      <div>
-        <GMap
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `72vh` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-          onClick={this.onClick}
-        />
+      <div className="main-page">
+        <InfoSection />
+        <div className="gmap">
+          <GMap
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `72vh` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+            onClick={this.onClick}
+          >
+            {this.state.circleCenter !== undefined
+              ? this.renderCircle(this.state.circleCenter)
+              : null}
+          </GMap>
+        </div>
       </div>
     );
   }
--- src/gmap/gmap.js
+++ src/gmap/gmap.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { withGoogleMap, GoogleMap } from 'react-google-maps';
 
-const hongkong = { lat: 22.3964, lng: 114.1095 };
+const hongkong = { lat: 22.3064, lng: 114.1795 };
 
 const GMap = withGoogleMap(props => (
   <GoogleMap
     id="map"
-    defaultZoom={11}
+    defaultZoom={12}
     defaultCenter={hongkong}
     onClick={props.onClick}
   >
