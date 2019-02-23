9ca37d054cc6cabeaa08de0582a5a92dbbc2a1d4
tyleung <tyleung@ualberta.ca>
2019-02-24 00:18:16 +0800

    Add circle

---
+++ src/infoSection.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class InfoSection extends Component {
+  render() {
+    return <div className="info-section">charts</div>;
+  }
+}
+
+export default InfoSection;
