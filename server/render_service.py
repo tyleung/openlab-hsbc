5347896b5a5acdf57684bb1bb998338a8139e076
cchencool <cchencool@gmail.com>
2019-02-24 05:55:07 +0800

    add real-time monitoring

--- server/render_service.py
+++ server/render_service.py
@@ -9,6 +9,7 @@
 
 __author__ = "chen"
 
+import logging
 
 # {
 #     "title": {
@@ -36,15 +37,52 @@ __author__ = "chen"
 #     }]
 # }
 
+logger = logging.getLogger(__name__)
 
 class Echart_Graph(object):
 
-  def __init__(self):
+  def __init__(self, type='common'):
     self.title = {"text": "The Chart", "left": "center"}
     self.textStyle = {"color":"#111", "fontStyle": "normal", "fontWeight": "bold", "fontFamily": "sans-serif", "fontSize": 22}
-    self.xAxis = {"type": "category", "data": []}
-    self.yAxis = {"type": "value"}
-    self.series = [{"data":[], "type": "line", "name": "", "color": "red", "showSymbol": "false", "hoverAnimation": "false"}]
+    self.tooltip = {"trigger": "axis", "axisPointer": {"animation": False}, "formatter": "function(params) {return params[0].value}"}
+    self.legend = {"x": "center", "y": "bottom", "data": []}
+    if type == 'common':
+      self.xAxis = {"data": [0, 1, 2, 3, 4], "type": "category", "splitLine": {"show": False}}
+      self.yAxis = {"type": "value", "splitLine": {"show": False}}
+      self.series = [{"data":[1, 2, 1, 3, 4], "type": "line", "name": "", "color": "red", "showSymbol": False, "hoverAnimation": False}]
+    elif type == 'bar':
+      self.toolbox = {
+        "show": True,
+        "feature": {
+            "mark": {"show": True},
+            "dataView": {"show": True, "readOnly": False},
+            "magicType": {
+                "show": True,
+                "type": ["pie", "funnel"]
+            },
+            "restore": {"show": True},
+            "saveAsImage": {"show": True}
+        }
+      }
+      self.calculable = True
+      self.series = {
+            "name":"面积模式",
+            "type":"pie",
+            "radius": [30, 110],
+            "center": ["50%", "50%"],
+            "roseType": "area",
+            "data":[
+                {"value":10, "name":"rose1"},
+                {"value":5, "name":"rose2"},
+                {"value":15, "name":"rose3"},
+                {"value":25, "name":"rose4"},
+                {"value":20, "name":"rose5"},
+                {"value":35, "name":"rose6"},
+                {"value":30, "name":"rose7"},
+                {"value":40, "name":"rose8"}
+            ]
+        }
+
 
   def to_dict(self):
     s = dict()
@@ -60,5 +98,23 @@ class Graph_Formatter(object):
   def __init__(self):
     pass
 
-  def get_graph(self, data=[]):
-    return Echart_Graph().to_dict()
+  def get_graph(self, title_text="", xdata=[], ydata=[], series_type='line', xtype='category'):
+    logger.info("start rendering graph...")
+    graph = Echart_Graph()
+    if len(xdata) > 0:
+      graph.xAxis['data'] = xdata
+      logger.info("xdata setted...")
+    if len(ydata) > 0:
+      graph.series[0]['data'] = ydata
+      logger.info("ydata setted...")
+    if title_text:
+      graph.title['text'] = title_text
+      logger.info("title text setted...")
+    if series_type != 'pi':
+      graph.series[0]['type'] = series_type
+      logger.info("series type setted...")
+    if xtype:
+      graph.xAxis['type'] = xtype
+      logger.info("xaxis type setted...")
+    logger.info("finish rendering graph!")
+    return graph.to_dict()
